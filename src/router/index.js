a85db5995323ddf432c66e7de34be66c5930f2b9
DimasAfifRehardendi <dimas_afif_27rpl@student.smktelkom-mlg.sch.id>
2020-04-02 21:19:00 +0700

    penambahan vue login & register

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Daily from '../views/Daily.vue'
+import Navbar from '../views/layouts/Navbar.vue'
+import Footer from '../views/layouts/Footer.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/daily',
+    name: 'daily',
+    components: {default: Daily, header: Navbar, footer: Footer},
+    meta: { 
+      requiresAuth: true
+    }
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
 })
 
 export default router
